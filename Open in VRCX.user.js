6e741056180b73e1f8f9f77ecd8d7d023ee63ba4
Myrkur <Myrkur@myrkur.dev>
2024-09-27 02:41:45 -0400

    Optimized code
    optimized observer mutations, add world button aswell.

--- Open in VRCX.user.js
+++ Open in VRCX.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Open in VRCX
 // @namespace    http://tampermonkey.net/
-// @version      1.3.5
+// @version      1.3.6
 // @updateURL    https://raw.githubusercontent.com/Myrkie/open-in-vrcx/mistress/Open%20in%20VRCX.user.js?
 // @downloadURL  https://raw.githubusercontent.com/Myrkie/open-in-vrcx/mistress/Open%20in%20VRCX.user.js?
 // @description  Adds an "Open in VRCX" button to user profiles and avatars;
@@ -14,59 +14,69 @@
 
 (function() {
     'use strict';
-    let UserButton;
-    let AvatarButton;
+
+    let UserButton, AvatarButton, WorldButton;
+    let debounceTimer;
+
+    function debounce(fn, delay) {
+        return function(...args) {
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(() => fn.apply(this, args), delay);
+        };
+    }
 
     function addButtonToNavbar() {
         const currentURL = window.location.href;
-
-
         switch (true) {
             case currentURL.includes("/home/user/"):
-                addUserButton();
-                if(AvatarButton != null){
-                    AvatarButton.remove();
+                if (!document.querySelector('#OpenUserinVRCX')) {
+                    addUserButton();
                 }
+                removeButton(AvatarButton);
+                removeButton(WorldButton);
                 break;
             case currentURL.includes("/home/avatar/"):
-                addAvatarButton();
-                if(UserButton != null){
-                    UserButton.remove();
+                if (!document.querySelector('#OpenAvatarinVRCX')) {
+                    addAvatarButton();
                 }
+                removeButton(UserButton);
+                removeButton(WorldButton);
+                break;
+            case currentURL.includes("/home/world/"):
+                if (!document.querySelector('#OpenWorldinVRCX')) {
+                    addWorldButton();
+                }
+                removeButton(UserButton);
+                removeButton(AvatarButton);
                 break;
             default:
-                if(UserButton != null){
-                    UserButton.remove();
-                }
-                if(AvatarButton != null){
-                    AvatarButton.remove();
-                }
+                removeButton(UserButton);
+                removeButton(AvatarButton);
+                removeButton(WorldButton);
                 break;
         }
     }
 
+    function removeButton(button) {
+        if (button) {
+            button.remove();
+        }
+    }
 
-    function addUserButton(){
+    function addUserButton() {
         let navbarSection = document.querySelector('.navbar-section.left-nav');
 
-        if (navbarSection && !document.querySelector('#OpenUserinVRCX')) {
+        if (navbarSection) {
             UserButton = document.createElement('button');
             UserButton.id = 'OpenUserinVRCX';
             UserButton.innerText = 'Open User in VRCX';
 
             UserButton.classList.add('p-2', 'btn', 'navbar-btn', 'medium');
-
-            let vrcsettingsSVG = document.querySelector('.svg-inline--fa.fa-gears.fa-lg.css-1efeorg.e9fqopp0');
-            if (vrcsettingsSVG) {
-                let svgClone = vrcsettingsSVG.cloneNode(true);
-                svgClone.style.marginRight = '8px';
-                UserButton.insertBefore(svgClone, UserButton.firstChild);
-            }
+            addSVGIcon(UserButton);
 
             UserButton.onclick = function() {
-                var trimid = window.location.href.lastIndexOf('/');
-                var parsedid = window.location.href.substring(trimid + 1);
-                let uriPath = new URL("vrcx://user/" + parsedid);
+                const parsedId = window.location.href.split('/').pop();
+                const uriPath = new URL(`vrcx://user/${parsedId}`);
                 window.open(uriPath, '_self');
             };
 
@@ -74,27 +84,20 @@
         }
     }
 
-    function addAvatarButton(){
+    function addAvatarButton() {
         let navbarSection = document.querySelector('.navbar-section.left-nav');
 
-        if (navbarSection && !document.querySelector('#OpenAvatarinVRCX')) {
+        if (navbarSection) {
             AvatarButton = document.createElement('button');
             AvatarButton.id = 'OpenAvatarinVRCX';
             AvatarButton.innerText = 'Open Avatar in VRCX';
 
             AvatarButton.classList.add('p-2', 'btn', 'navbar-btn', 'medium');
-
-            let vrcsettingsSVG = document.querySelector('.svg-inline--fa.fa-gears.fa-lg.css-1efeorg.e9fqopp0');
-            if (vrcsettingsSVG) {
-                let svgClone = vrcsettingsSVG.cloneNode(true);
-                svgClone.style.marginRight = '8px';
-                AvatarButton.insertBefore(svgClone, AvatarButton.firstChild);
-            }
+            addSVGIcon(AvatarButton);
 
             AvatarButton.onclick = function() {
-                var trimid = window.location.href.lastIndexOf('/');
-                var parsedid = window.location.href.substring(trimid + 1);
-                let uriPath = new URL("vrcx://avatar/" + parsedid);
+                const parsedId = window.location.href.split('/').pop();
+                const uriPath = new URL(`vrcx://avatar/${parsedId}`);
                 window.open(uriPath, '_self');
             };
 
@@ -102,18 +105,41 @@
         }
     }
 
-    function awa() {
-        console.log("awa");
+    function addWorldButton() {
+        let navbarSection = document.querySelector('.navbar-section.left-nav');
+
+        if (navbarSection) {
+            WorldButton = document.createElement('button');
+            WorldButton.id = 'OpenWorldinVRCX';
+            WorldButton.innerText = 'Open World in VRCX';
+
+            WorldButton.classList.add('p-2', 'btn', 'navbar-btn', 'medium');
+            addSVGIcon(WorldButton);
+
+            WorldButton.onclick = function() {
+                const parsedId = window.location.href.split('/').pop();
+                const uriPath = new URL(`vrcx://world/${parsedId}`);
+                window.open(uriPath, '_self');
+            };
+
+            navbarSection.appendChild(WorldButton);
+        }
     }
 
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach(() => {
-            addButtonToNavbar();
-        });
-    });
+    function addSVGIcon(button) {
+        let vrcsettingsSVG = document.querySelector('.svg-inline--fa.fa-gears.fa-lg.css-1efeorg.e9fqopp0');
+        if (vrcsettingsSVG) {
+            let svgClone = vrcsettingsSVG.cloneNode(true);
+            svgClone.style.marginRight = '8px';
+            button.insertBefore(svgClone, button.firstChild);
+        }
+    }
 
-    observer.observe(document.body, { childList: true, subtree: true });
+    const observer = new MutationObserver(debounce((mutations) => {
+        addButtonToNavbar();
+    }, 500));
+
+    observer.observe(document.querySelector('.navbar-section.left-nav') || document.body, { childList: true, subtree: true });
 
     window.addEventListener('load', addButtonToNavbar);
-    window.addEventListener('popstate', awa);
 })();
